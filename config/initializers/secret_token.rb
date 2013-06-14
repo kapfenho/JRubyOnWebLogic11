282755261a18c3456304f447ea6b45d8cab5161e
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2013-06-14 11:02:06 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JRubyOnWebLogic11::Application.config.secret_token = '3ca80c6debaaa393249f678194789b5eb6c46f3e060145f047af7c14d60ecd094d250d64d7ebd06a89af3d008cfddc4753897e46ca94f78aa92815ecfa8073cb'
